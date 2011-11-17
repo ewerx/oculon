e580c7a5af1ea0c5aae257b14ae2e9e656306c77
ehsan <ehsan@ewerx.ca>
2011-11-17 00:54:26 -0500

    TextEntity: added justification, cleanup

--- src/TextEntity.cpp
+++ src/TextEntity.cpp
@@ -10,20 +10,27 @@
 #include "TextEntity.h"
 #include "cinder/Text.h"
 #include "cinder/gl/Texture.h"
+#include "cinder/app/App.h"
 
 using namespace std;
 using namespace ci;
 
 TextEntity::TextEntity()
 : mString("")
-//, mFontName("Arial-BoldMT")
-//, mFontSize(12.0f)
-//, mTextColor(ColorA(1.0f,1.0f,1.0f,1.0f))
+, mJustifyRight(false)
+, mJustifyBottom(false)
+, mMarginRight(10.0f)
+, mMarginBottom(10.0f)
 {
 }
 
 TextEntity::~TextEntity()
 {
+}
+
+void TextEntity::resize()
+{
+    updateTextSize();
 }
 
 void TextEntity::update(double dt)
@@ -59,53 +66,43 @@ void TextEntity::draw()
 
 void TextEntity::setText( const std::string& str )
 {
-    //mString = str;
-    //createTexture();
     mTextBox.setText(str);
+    updateTextSize();
 }
 
 void TextEntity::setText( const std::string& str, const ci::ColorA& color )
 {
-    //mTextColor = color;
-    //setText(str);
-    mTextBox.setText(str);
+    setText(str);
     mTextBox.setColor(color);
 }
 
 void TextEntity::setText( const std::string& str, const std::string& font, const float size, const ci::ColorA& color )
 {
-    //mFontName = font;
-    //mFontSize = size;
-    //mTextColor = color;
-    //setText(str);
-    mTextBox.setText(str);
+    setText(str);
     mTextBox.setColor(color);
     mTextBox.setFont(Font( font, size ));
 }
 
 void TextEntity::setTextColor( const ci::ColorA& color )
 {
-    //mTextColor = color;
-    //createTexture();
     mTextBox.setColor(color);
 }
 
 void TextEntity::setFont( const std::string& fontName, const float fontSize )
 {
-    //mFontName = fontName;
-    //createTexture();
     mTextBox.setFont(Font(fontName,fontSize));
+    updateTextSize();
 }
 
-/*
-void TextEntity::createTexture()
+void TextEntity::updateTextSize()
 {
-    TextLayout layout;
+    if( mJustifyRight )
+    {
+        mPosition.x = app::getWindowWidth() - mTextBox.measure().x - mMarginRight;
+    }
     
-    layout.setFont( Font( mFontName, mFontSize ) );
-    layout.setColor( mTextColor );
-    layout.addLine( mString );
-    
-    mTexture = gl::Texture( layout.render( true ) ); //TODO: check for memory leak??
+    if( mJustifyBottom )
+    {
+        mPosition.y = app::getWindowHeight() - mTextBox.measure().y - mMarginBottom;
+    }
 }
-*/
--- src/TextEntity.h
+++ src/TextEntity.h
@@ -28,23 +28,27 @@ public:
     void update(double dt);
     void draw();
     
+    void resize(); //TODO: make this part of Entity
+    
     // new
     void setText( const std::string& str );
     void setText( const std::string& str, const ci::ColorA& color );
     void setText( const std::string& str, const std::string& font, const float size, const ci::ColorA& color );
     void setTextColor( const ci::ColorA& color );
     void setFont( const std::string& fontName, const float fontSize );
+    void setRightJustify( bool on, float margin = 0.0f )    { mJustifyRight = on; mMarginRight = margin; }
+    void setBottomJustify( bool on, float margin = 0.0f )   { mJustifyBottom = on; mMarginBottom = margin; }
     
 private:
-    void createTexture();
+    void updateTextSize();
     
 private:
     std::string         mString;
-    //std::string         mFontName;
-    //float               mFontSize;
-    //ci::ColorA          mTextColor;
-    //ci::gl::Texture     mTexture;
     TextBox             mTextBox;
+    bool                mJustifyRight;
+    bool                mJustifyBottom;
+    float               mMarginRight;
+    float               mMarginBottom;
 };
 
 #endif // __TEXTENTITY_H__
