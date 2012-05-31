6f21cd28f7a286ded49c36f44da4eab1edc7293a
ehsan <ehsan@ewerx.ca>
2012-05-31 00:47:14 -0400

    Signal: make parameters adjustable, tweaks

---
+++ src/test/SignalScope.h
@@ -0,0 +1,74 @@
+/*
+ *  SignalScope.h
+ *  Oculon
+ *
+ *  Created by Ehsan on 11-10-22.
+ *  Copyright 2011 ewerx. All rights reserved.
+ *
+ */
+
+#pragma once
+
+#include "cinder/Timeline.h"
+#include "cinder/PolyLine.h"
+
+class Scene;
+
+//TODO: refactor as SubScene
+class SignalScope
+{
+public:
+    SignalScope(Scene* scene);
+    virtual ~SignalScope();
+
+    void setup();
+    void setupInterface();
+    void setupDebugInterface();
+    void resize();
+    void reset();
+    void update(double dt);
+    void draw();
+    
+private:
+    enum { NUM_POINTS = 512 };
+    
+    int mNumLines;
+    
+    float mLineWidth;
+    float mScale;
+    float mFallOffMin;
+    float mFallOffMax;
+    float mHorizSmoothingMin;
+    float mHorizSmoothingMax;
+    float mSignalScale;
+    float mSignalMaxRatio;
+    int mCenterBiasRange;
+    bool mRandomizeSignal;
+    
+    enum eSignal
+    {
+        SIGNAL_AMPLITUDE,
+        SIGNAL_REAL,
+        SIGNAL_IMAGINARY,
+        SIGNAL_COUNT
+    };
+    int mSignalType;
+    
+    enum eSignalSelection
+    {
+        SIGNAL_RANDOM,
+        SIGNAL_ONE,
+    };
+    int mSignalSelection;
+    
+    struct tLine
+    {
+        tLine() : mValue( NUM_POINTS, 0.0f ) {}
+        std::vector<float> mValue;
+        ci::PolyLine2f mPolyLine;
+    };
+    std::vector<tLine> mLines;
+    
+    Scene*  mParentScene;
+
+};
