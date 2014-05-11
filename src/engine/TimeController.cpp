182f8fc863ca5bf08b188c0a057f409eacdf8cb7
Ehsan <ehsan@ewerx.ca>
2014-05-11 19:30:44 -0400

    add TimerController class

---
+++ src/engine/TimeController.cpp
@@ -0,0 +1,60 @@
+//
+//  TimeController.cpp
+//  Oculon
+//
+//  Created by Ehsan Rezaie on 2014-05-11.
+//
+//
+
+#include "TimeController.h"
+
+#pragma mark - constructor/destructor
+
+TimeController::TimeController()
+: mElapsedSeconds(0.0f)
+, mDelta(0.0f)
+, mTimeScale(1.0f)
+, mTick(false)
+, mTickTimer(0.0f)
+, mTickDuration(1.0f)
+{
+}
+
+TimeController::~TimeController()
+{
+}
+
+#pragma mark - setup
+
+void TimeController::reset()
+{
+    mElapsedSeconds = 0.0f;
+    mDelta = 0.0f;
+    mTickTimer = 0.0f;
+    mTick = false;
+}
+
+void TimeController::setupInterface(Interface *interface, const std::string &name)
+{
+    interface->addParam(CreateFloatParam("timescale", &mTimeScale)
+                        .maxValue(100.0f)
+                        .oscReceiver(name));
+}
+
+#pragma mark - update
+
+void TimeController::update(double dt)
+{
+    mDelta = dt * mTimeScale;
+    
+    mElapsedSeconds += mDelta;
+    
+    mTickTimer += mDelta;
+    mTick = false;
+    if (mTickTimer > mTickDuration)
+    {
+        mTickTimer = 0.0f;
+        mTick = true;
+        mTickSignal();
+    }
+}
---
+++ src/engine/TimeController.h
@@ -0,0 +1,51 @@
+//
+//  TimeController.h
+//  Oculon
+//
+//  Created by Ehsan Rezaie on 2014-05-11.
+//
+//
+
+#pragma once
+
+#include "AudioInputHandler.h"
+#include "EaseCurveSelector.h"
+#include "Interface.h"
+
+#include "cinder/Timeline.h"
+
+#include <vector>
+#include <boost/signals2.hpp>
+
+
+class TimeController
+{
+public:
+    typedef boost::signals2::signal<void()> TriggerSignal;
+
+public:
+    TimeController();
+    virtual ~TimeController();
+    
+    void setupInterface( Interface* interface, const std::string& name );
+    void reset();
+    void update(double dt);
+
+    TriggerSignal& getTickSignal()      { return mTickSignal; }
+    double getElapsedSeconds() const    { return mElapsedSeconds; }
+    double getDelta() const             { return mDelta; }
+    bool getTick()                      { return mTick; }
+    void setTickDuration(float seconds) { mTickDuration = seconds; }
+    
+private:
+    double mElapsedSeconds;
+    double mDelta;
+    float mTimeScale;
+    
+    // tick
+    bool mTick;
+    double mTickTimer;
+    float mTickDuration;
+    TriggerSignal mTickSignal;
+    
+};
